96a9d3f4074925de672994247784d8168d5ab910
Khoa Pham <phamnhatkhoa1908@gmail.com>
2022-03-27 18:04:22 +0700

    update UI login screen

--- ToDo/app/src/main/java/com/example/todo/FirstLogin.java
+++ ToDo/app/src/main/java/com/example/todo/FirstLogin.java
@@ -1,14 +1,43 @@
 package com.example.todo;
 
+import android.app.ActionBar;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class FirstLogin extends AppCompatActivity {
-
+    Button loginButton;
+Button signUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_login);
+        setButtonScreen();
+    }
+
+    void setButtonScreen() {
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- ToDo/app/src/main/java/com/example/todo/Login.java
+++ ToDo/app/src/main/java/com/example/todo/Login.java
@@ -1,8 +1,14 @@
 package com.example.todo;
 
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
 
@@ -11,4 +17,5 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
 }
