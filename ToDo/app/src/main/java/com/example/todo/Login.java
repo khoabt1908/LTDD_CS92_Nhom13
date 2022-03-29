1541aeae0d89391e6c162cf2b26f4e3e6cda824e
Khoa Pham <phamnhatkhoa1908@gmail.com>
2022-03-29 15:11:26 +0700

    fix keyboard demiss

--- ToDo/app/src/main/java/com/example/todo/Login.java
+++ ToDo/app/src/main/java/com/example/todo/Login.java
@@ -2,10 +2,10 @@ package com.example.todo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CheckedTextView;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +13,31 @@ import androidx.core.text.HtmlCompat;
 
 public class Login extends AppCompatActivity {
     TextView textViewDontHaveAccount;
+    LinearLayout mainContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         textViewDontHaveAccount = (TextView) findViewById(R.id.dontHaveAccount);
+        mainContainer = (LinearLayout) findViewById(R.id.mainContainer);
+        Init();
 
+    }
+
+    void Init() {
+        textViewDontHaveAccount.setText(HtmlCompat.fromHtml("<font color=#666666>Didn't have an account</font> <u></font><b><font color=#0173B7>Sign Up</font></u></b></font>", 0));
+        mainContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+
+                if (imm.isAcceptingText()) { // verify if the soft keyboard is open
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
         textViewDontHaveAccount.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -27,10 +46,5 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        Init();
-
-    }
-    void Init() {
-        textViewDontHaveAccount.setText(HtmlCompat.fromHtml("<font color=#666666>Didn't have an account</font> <u></font><b><font color=#0173B7>Sign Up</font></u></b></font>",0));
     }
 }
--- ToDo/app/src/main/java/com/example/todo/Register.java
+++ ToDo/app/src/main/java/com/example/todo/Register.java
@@ -3,7 +3,10 @@ package com.example.todo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,8 @@ import androidx.core.text.HtmlCompat;
 public class Register extends AppCompatActivity {
     TextView textViewPolicy;
     TextView textViewHaveAccount;
+    LinearLayout mainContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +24,31 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         textViewPolicy = (TextView) findViewById(R.id.confirm);
         textViewHaveAccount = (TextView) findViewById(R.id.haveAccount);
+        mainContainer = (LinearLayout) findViewById(R.id.mainContainer);
 
-        textViewHaveAccount.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-            }
-        });
         Init();
 
     }
     void Init() {
         textViewPolicy.setText(HtmlCompat.fromHtml("<font color=#666666>By signing up you accept the </font><font color=#0173B7>  <b><u>Terms & Conditions</u></b></font><font color=#666666> and the <u></font><b><font color=#0173B7>Privacy Policy</font></u></b></font>",0));
         textViewHaveAccount.setText(HtmlCompat.fromHtml("<font color=#666666>Already have an account</font> <u></font><b><font color=#0173B7>Log In</font></u></b></font>",0));
+        textViewHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+        mainContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+
+                if (imm.isAcceptingText()) { // verify if the soft keyboard is open
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
     }
 }
