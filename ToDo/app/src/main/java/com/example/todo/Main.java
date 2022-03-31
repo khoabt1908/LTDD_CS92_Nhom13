3c867cef8f62dd18a0bb33422d84f3cefe0d8ba2
ngocng <nguyenhoangnhungoc.1411@gmail.com>
2022-03-31 13:26:02 +0700

    update UI draw nav

--- ToDo/app/src/main/java/com/example/todo/Main.java
+++ ToDo/app/src/main/java/com/example/todo/Main.java
@@ -1,14 +1,27 @@
 package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.navigation.NavigationView;
 
 public class Main extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
