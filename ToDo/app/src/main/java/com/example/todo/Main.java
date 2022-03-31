b9ebe7ca0fa8ae33edfcf076a4a875ec25193576
Khoa Pham <phamnhatkhoa1908@gmail.com>
2022-03-31 14:17:36 +0700

    fix drawer nav

--- ToDo/app/src/main/java/com/example/todo/Main.java
+++ ToDo/app/src/main/java/com/example/todo/Main.java
@@ -1,27 +1,42 @@
 package com.example.todo;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.Toolbar;
-
-import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class Main extends AppCompatActivity {
-
-
+    private MaterialToolbar topAppBar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        topAppBar = (MaterialToolbar) findViewById(R.id.topAppBar);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
 
+        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.close();
+                return true;
+            }
+        });
     }
 }
