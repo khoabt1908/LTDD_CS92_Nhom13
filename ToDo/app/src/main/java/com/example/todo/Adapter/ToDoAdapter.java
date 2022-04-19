1baacebd904dd72fabb8e063a3b5db02a08c888a
Khoa Pham <phamnhatkhoa1908@gmail.com>
2022-04-20 00:05:57 +0700

    update checkbox strikethrough

--- ToDo/app/src/main/java/com/example/todo/Adapter/ToDoAdapter.java
+++ ToDo/app/src/main/java/com/example/todo/Adapter/ToDoAdapter.java
@@ -1,15 +1,13 @@
 package com.example.todo.Adapter;
 
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.todo.Main;
 import com.example.todo.ManageTaskFragment;
 import com.example.todo.Model.TaskModel;
 import com.example.todo.R;
@@ -24,35 +22,50 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
         this.todoList = todoList;
     }
 
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-            View itemView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.task_layout, parent, false);
-            return new ViewHolder(itemView);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
     }
 
-    public void onBindViewHolder(ViewHolder holder, int position){
+    public void onBindViewHolder(ViewHolder holder, int position) {
         TaskModel item = todoList.get(position);
         holder.task.setText(item.getTask());
         holder.task.setChecked(toBoolean(item.getStatus()));
+        if (toBoolean(item.getStatus()))
+            holder.task.setPaintFlags(holder.task.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        holder.task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(holder.task.isChecked()){
+                    holder.task.setPaintFlags(holder.task.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                }
+                else {
+                    holder.task.setPaintFlags(holder.task.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                }
+
+            }
+        });
     }
 
-    public void setTasks(List<TaskModel> todoList){
+    public void setTasks(List<TaskModel> todoList) {
         this.todoList = todoList;
         notifyDataSetChanged();
     }
 
-    public int getItemCount(){
+    public int getItemCount() {
         return todoList.size();
     }
 
-    private Boolean toBoolean(int n){
+    private Boolean toBoolean(int n) {
         return n != 0;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         CheckBox task;
 
-        ViewHolder(View view){
+        ViewHolder(View view) {
             super(view);
             task = view.findViewById(R.id.todoCheckBox);
         }
--- ToDo/app/src/main/java/com/example/todo/ManageTaskFragment.java
+++ ToDo/app/src/main/java/com/example/todo/ManageTaskFragment.java
@@ -2,17 +2,16 @@ package com.example.todo;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todo.Adapter.ToDoAdapter;
 import com.example.todo.Model.TaskModel;
@@ -28,17 +27,14 @@ import java.util.List;
  */
 public class ManageTaskFragment extends Fragment {
 
-    private FloatingActionButton addTask;
-    private RecyclerView recyclerView;
-    private ToDoAdapter taskAdapter;
-
-    private List<TaskModel> taskList;
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private FloatingActionButton addTask;
+    private RecyclerView recyclerView;
+    private ToDoAdapter taskAdapter;
+    private List<TaskModel> taskList;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -91,24 +87,20 @@ public class ManageTaskFragment extends Fragment {
         recyclerView.setAdapter(taskAdapter);
 
         TaskModel task = new TaskModel();
-        task.setTask("Báo mới nè");
+        task.setTask("task 1aaaaaaaaaaaaaaaaa");
         task.setStatus(0);
         task.setId(1);
-
         TaskModel task2 = new TaskModel();
-        task2.setTask("Chỉ đi năn nỉ");
+        task2.setTask("task 1");
         task2.setStatus(1);
-        task2.setId(2);
+        task.setId(1);
 
 
-
         taskList.add(task);
         taskList.add(task);
         taskList.add(task);
         taskList.add(task);
         taskList.add(task);
-        taskList.add(task);
-        taskList.add(task2);
         taskList.add(task2);
         taskList.add(task2);
         taskList.add(task2);
