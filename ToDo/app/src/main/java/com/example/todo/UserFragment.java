afa59354f6ec6baaebc8ed21fe78a2e782680d28
nguyenhoangnhungoc <nguyenhoangnhungoc.1411@gmail.com>
2022-05-05 15:03:46 +0700

    update layout user

--- ToDo/app/src/main/java/com/example/todo/UserFragment.java
+++ ToDo/app/src/main/java/com/example/todo/UserFragment.java
@@ -59,6 +59,9 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
 }
